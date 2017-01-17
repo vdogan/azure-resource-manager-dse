9d963ef2321aac1754de4da98c3e2f7fd9d47652
benofben <ben.lackey@outlook.com>
2017-01-17 09:42:02 -0800

    suspicions about re-entrance

--- extensions/opsCenter.sh
+++ extensions/opsCenter.sh
@@ -1,36 +1,3 @@
 #!/usr/bin/env bash
 
-username=$1
-password=$2
-
-echo "Input to node.sh is:"
-echo username $username
-echo password $password
-
-public_ip=`curl --retry 10 icanhazip.com`
-cluster_name="mycluster"
-
-echo "Calling setupCluster.py with the settings:"
-echo public_ip $public_ip
-echo cluster_name $cluster_name
-echo username $username
-echo password $password
-
-apt-get update
-apt-get -y install unzip python-pip
-pip install requests
-
-cd /
-wget https://github.com/DSPN/install-datastax-ubuntu/archive/lcm.zip
-unzip lcm.zip
-cd install-datastax-ubuntu-lcm/bin
-
-./os/install_java.sh
-./opscenter/install.sh
-./opscenter/start.sh
-
-./lcm/setupCluster.py \
---opsc-ip $public_ip \
---clustername $cluster_name \
---user $username \
---password $password
+echo "I just ran once."
